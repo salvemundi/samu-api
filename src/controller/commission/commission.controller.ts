a031fca3d29e67731ebacb399f557e241e3f9799
CPP-MASTERRACE <jeffrexyv@gmail.com>
2019-06-22 01:17:01 +0200

    Added basic swagger api docs.

--- src/controller/commission/commission.controller.ts
+++ src/controller/commission/commission.controller.ts
@@ -2,7 +2,8 @@ import { Controller, Post, Body, HttpCode } from '@nestjs/common';
 import { CommissionService } from '../../service/commission/commission.service';
 import { Commission } from '../../entity/Commission.entity';
 import { CreateCommissionDto } from '../../../dist/dto/commission/create-commission-dto';
-
+import { ApiUseTags } from '@nestjs/swagger';
+@ApiUseTags('Commissions')
 @Controller('commissions')
 export class CommissionController {
     constructor(private readonly commisionService: CommissionService) {
--- src/dto/commission/create-commission-dto.ts
+++ src/dto/commission/create-commission-dto.ts
@@ -1,12 +1,16 @@
 import { IsNotEmpty, Max, IsDate, MaxLength } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateCommissionDto {
+  @ApiModelProperty({required: true, maxLength: 255})
   @MaxLength(255)
   name: string;
 
+  @ApiModelProperty({required: true})
   @IsNotEmpty()
   description: string;
 
+  @ApiModelProperty({required: true, format: 'datetime'})
   @IsNotEmpty()
   created: Date;
 }
