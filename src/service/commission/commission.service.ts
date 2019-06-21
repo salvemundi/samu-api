95d5ae5c309136f466d474d4125394668bd83b28
CPP-MASTERRACE <jeffrexyv@gmail.com>
2019-06-22 01:41:37 +0200

    Added correct return types.

--- src/service/commission/commission.service.ts
+++ src/service/commission/commission.service.ts
@@ -7,18 +7,18 @@ export class CommissionService {
         await Commission.save(model);
     }
 
-    public async read(skip: number, take: number) {
+    public async read(skip: number, take: number): Promise<Commission[]> {
         if (take > 100) {
             take = 1;
         }
         return await Commission.find({skip, take});
     }
 
-    public async readOne(id: number) {
+    public async readOne(id: number): Promise<Commission> {
         return await Commission.findOne(id);
     }
 
-    public async update(model: Commission) {
+    public async update(model: Commission): Promise<void> {
         await Commission.save(model);
     }
 }
