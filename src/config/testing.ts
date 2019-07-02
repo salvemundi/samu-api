4c949be5d7b465b3549dfa0d81ed8033642c84a0
INeedSugar <bastiankpn7800@gmail.com>
2019-07-02 23:02:11 +0200

    Auto stash before merge of "feature/travisci" and "origin/feature/travisci"

--- src/config/testing.ts
+++ src/config/testing.ts
@@ -1,7 +1,18 @@
-import { Connection } from "typeorm";
+import { createConnection } from 'typeorm';
 
 export class DatabaseSeeder {
-    public before() {
-        // Connection
+    public async before() {
+        const connection = await createConnection({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'my-secret-pw',
+            migrations: ['./../migration/*.js'],
+        });
+
+        connection.query('CREATE DATABSE test IF NOT EXISTS;');
+        connection.query('USE test;');
+        await connection.runMigrations({transaction: true});
     }
-} 
+}
--- src/controllers/commission/commission.controller.spec.ts
+++ src/controllers/commission/commission.controller.spec.ts
@@ -1,18 +1,30 @@
 import { CommissionController } from './commission.controller';
 import { CommissionService } from './../../services/commission/commission.service';
+import { CreateCommissionDto } from './../../dto/commission/create-commission-dto';
+import { DatabaseSeeder } from './../../config/testing';
 
 describe('Commission Controller', () => {
   let controller: CommissionController;
   let service: CommissionService;
+  let database: DatabaseSeeder;
 
   beforeAll(async () => {
+    database = new DatabaseSeeder();
+    await database.before();
+
     service = new CommissionService();
     controller = new CommissionController(service);
   });
 
   it('Can read all commissions', async () => {
     const result = [];
-    jest.spyOn(service, 'read').mockImplementation(() => Promise.resolve(result));
+    // jest.spyOn(service, 'read').mockImplementation(() => Promise.resolve(result));
+
+    const create = new CreateCommissionDto();
+    create.name = "Bastiaan";
+    create.created = new Date();
+    create.description = "reee";
+    await controller.create(create);
 
     expect((await controller.readAll(0, 100)).commissions).toBe(result);
   });
