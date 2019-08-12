0c9ebb88a43ef71f26c3eb2e900334e9a8ee93f0
INeedSugar <bastiankpn7800@gmail.com>
2019-08-12 22:46:59 +0200

    Other approach

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "moduleDirectories": [ "node_modules", "src" ],
+    "moduleFileExtensions": [ "js", "json", "ts" ],
+    "roots": [ "src" ],
+    "testRegex": ".spec.ts$",
+    "transform": {
+      "^.+\\.ts$": "ts-jest"
+    },
+    "coverageDirectory": "../coverage",
+    "moduleNameMapper": {
+      "src/(.*)": "<rootDir>/src/$1"
+    }
+}
