662ff03b778875f38599d20850a311d2aa78b4ae
INeedSugar <bastiankpn7800@gmail.com>
2019-08-12 22:57:21 +0200

    Another try...........

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,6 @@ module.exports = {
     },
     "coverageDirectory": "../coverage",
     "moduleNameMapper": {
-      "src/(.*)": "<rootDir>/src/$0"
+      "src/(.*)": "<rootDir>/src/$0.ts"
     }
 }
