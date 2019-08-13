dd4f73500d52e03da8c09f03fc41d4537f0d744a
INeedSugar <bastiankpn7800@gmail.com>
2019-08-13 13:05:21 +0200

    Dto's are now documented

--- src/dto/member/create-member-dto.ts
+++ src/dto/member/create-member-dto.ts
@@ -1,35 +1,47 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class CreateMemberDto {
+    @ApiModelProperty({required: true})
     @IsNotEmpty()
     public pcn: number;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public firstName: string;
 
+    @ApiModelProperty({required: false, maxLength: 255})
     public middleName: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public lastName: string;
 
+    @ApiModelProperty({required: true})
     @IsNotEmpty()
     public birthday: Date;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public address: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public postalcode: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public city: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public country: string;
 
+    @ApiModelProperty({required: true, maxLength: 10})
     @IsNotEmpty()
     public phoneNumber: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public email: string;
 }
--- src/dto/member/update-member-dto.ts
+++ src/dto/member/update-member-dto.ts
@@ -1,38 +1,51 @@
 import { IsNotEmpty } from 'class-validator';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 export class UpdateMemberDto {
+    @ApiModelProperty({required: true})
     @IsNotEmpty()
     public id: number;
 
+    @ApiModelProperty({required: true})
     @IsNotEmpty()
     public pcn: number;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public firstName: string;
 
+    @ApiModelProperty({required: false, maxLength: 255})
     public middleName: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public lastName: string;
 
+    @ApiModelProperty({required: true})
     @IsNotEmpty()
     public birthday: Date;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public address: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public postalcode: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public city: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public country: string;
 
+    @ApiModelProperty({required: true, maxLength: 10})
     @IsNotEmpty()
     public phoneNumber: string;
 
+    @ApiModelProperty({required: true, maxLength: 255})
     @IsNotEmpty()
     public email: string;
 }
