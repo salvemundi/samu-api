8c77120e2eca50bc8951db125327441a7e8dd75d
INeedSugar <bastiankpn7800@gmail.com>
2019-08-13 16:51:48 +0200

    Added cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('Salve mundi API')
