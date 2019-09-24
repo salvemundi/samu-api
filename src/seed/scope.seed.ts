96592bed46ed32f8840ec05c9a2c61e19fad365b
Bastiaan Verhaar <bastiankpn7800@gmail.com>
2019-09-24 11:07:29 +0200

    Update scope.seed.ts

--- src/seed/scope.seed.ts
+++ src/seed/scope.seed.ts
@@ -6,7 +6,7 @@ import { getConnection, EntityManager } from 'typeorm';
 export class ScopeSeeder {
     private scopes: Scope[] = [
         new Scope('user:read', 1),
-        new Scope('user:wrie', 2),
+        new Scope('user:write', 2),
         new Scope('commission:read', 3),
         new Scope('commission:write', 4),
     ];
