9026c57cd05774554eca978400d210c0446ab21f
Davy de Haas <davy.dehaas98@gmail.com>
2019-10-15 14:42:01 +0200

    Convert from nestjs mailer to nodemailer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
 import { CommissionController } from './controllers/commission/commission.controller';
 import { CommissionService } from './services/commission/commission.service';
 import { MemberController } from './controllers/member/member.controller';
@@ -9,12 +8,12 @@ import { MemberModule } from './modules/member/member.module';
 import { typeormconfig } from './typeormconfig';
 import { UserService } from './services/user/user.service';
 import { UserController } from './controllers/user/user.controller';
+import { NodemailerModule } from 'nodemailer';
 import { mailerconfig } from './mailerconfig';
 
 @Module({
     imports: [
         TypeOrmModule.forRoot(typeormconfig),
-        MailerModule.forRoot(mailerconfig),
         MemberModule,
     ],
     controllers: [CommissionController, MemberController, UserController],
