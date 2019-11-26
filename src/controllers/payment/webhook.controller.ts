8f9e2fb7eab4a38e0bdfbdd563a068d00873fa86
Bastiaan Verhaar <verhaar.bastiaan@gmail.com>
2019-11-26 15:03:54 +0100

    Enabled logging webhook

--- src/controllers/payment/webhook.controller.ts
+++ src/controllers/payment/webhook.controller.ts
@@ -31,11 +31,16 @@ export class WebhookController {
         }
 
         if (payment.isPaid() && !payment.hasRefunds()) {
+            console.log(payment);
             await this.memberService.giveMembership(transaction.user);
+            console.log('membership given')
             await this.paymentService.transactionPaid(transaction);
+            console.log('transaction paid')
 
             const confirmation = await this.confirmationService.create(transaction.user);
+            console.log('confirmation created')
             await this.emailService.sendEmailConfirmationEmail(transaction.user, confirmation);
+            console.log('email send')
             return;
 
         } else if (payment.hasRefunds) {
