25a016b57ffb5a3801888accb5fae42c763bb728
Basiaan Verhaar <verhaar.bastiaan@gmail.coom>
2019-12-15 15:17:59 +0100

    Fix user entity

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -22,8 +22,8 @@ export class User extends BaseEntity {
     @ApiProperty()
     @Column()
     public lastName: string;
-  
-    @ApiModelProperty({ type: String, format: 'date' })
+
+    @ApiProperty({ type: String, format: 'date' })
     @Column()
     public birthday: Date;
 
@@ -47,11 +47,11 @@ export class User extends BaseEntity {
     @Column()
     public phoneNumber: string;
 
-    @ApiModelProperty()
+    @ApiProperty()
     @Column({ unique: true })
     public email: string;
 
-    @ApiModelProperty({ type: String, format: 'date' })
+    @ApiProperty({ type: String, format: 'date' })
     @Column()
     public registeredSince: Date;
 
