a92e0953c39c2700cd52cf19331d5644c804ec84
Bastiaan Verhaar <bastiankpn7800@gmail.coom>
2019-12-29 15:53:36 +0100

    Fix cookie maybe

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -34,7 +34,7 @@ export class AuthorizationGuard implements CanActivate {
 
       if (!!user && !!user.scopes.find(x => x.name === scope)) {
         const reponse = context.switchToHttp().getResponse();
-        reponse.cookie('auth', await this.authService.genJWT(user.id, user.email));
+        reponse.cookie('auth', await this.authService.genJWT(user.id, user.email), {secure: false, httpOnly: true});
         return true;
       }
 
